6ad859fab8d0ca9dcd2b429de479b6d1b01bd277
Felipe Santos <felipe.santos11@hotmail.com>
2021-07-18 18:55:28 -0300

    criacao de pipe customizavel

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,7 +10,8 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe
   ],
   imports: [
     BrowserModule,
---
+++ course-manager/src/app/pipe/replace.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+// elegível para ser um pipe
+@Pipe({
+  name: 'replace'
+})
+export class ReplacePipe implements PipeTransform {
+  // pipe para substituir qualquer coisa
+  transform(value: string, char: string, valueToReplace: string) {
+    return value.replace(char, valueToReplace);
+  }
+}
