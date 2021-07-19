1ced57e9eaadcd68b23c15b7276e55e5741d978d
Felipe Santos <felipe.santos11@hotmail.com>
2021-07-18 22:43:01 -0300

    add form, save method

--- course-manager/src/app/courses/course-info.component.ts
+++ course-manager/src/app/courses/course-info.component.ts
@@ -1,21 +1,31 @@
+import { Course } from 'src/app/courses/course';
+import { CourseService } from './course.service';
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  templateUrl: './course-info.component.html'
+  templateUrl: './course-info.component.html',
+  styleUrls: ['./course-info.component.css']
 })
 export class CourseInfoComponent implements OnInit {
-  courseId: number;
+  course: Course;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private service: CourseService) {
   }
 
   // + converte id string em number
   ngOnInit(): void {
-    this.courseId = +this.activatedRoute
-      .snapshot
-      .paramMap
-      .get('id');
+    this.course = this.service
+      .retrieveById(+this.activatedRoute
+        .snapshot
+        .paramMap
+        .get('id'));
+  }
+
+  save(): void {
+    this.service.save(this.course);
   }
 }
