488bbbea154b794d8915fc8fbcaf73b28cc76f6b
Felipe Santos <felipe.santos11@hotmail.com>
2021-07-19 20:36:19 -0300

    app segregado em modulos

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,37 +1,25 @@
-import { CourseInfoComponent } from './courses/course-info.component';
 import { Error404Component } from './error-404/error-404.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { StarComponent } from './star/star.component';
-import { CourseListComponent } from './courses/course-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
     Error404Component,
-    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent // se chamar 'courses', renderiza o component CourseList
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
       {
         path: '', redirectTo: 'courses', pathMatch: 'full' // rota raiz
       },
