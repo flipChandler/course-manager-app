15d38afc7d256e48c95394e1d89b37b6f8328fce
Felipe Santos <felipe.santos11@hotmail.com>
2021-07-19 22:06:52 -0300

    segregacao do error-404 para a pasta core

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,30 +1,23 @@
 import { CoreModule } from './core/core.module';
-import { Error404Component } from './error-404/error-404.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Error404Component,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     CourseModule,
     CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full' // rota raiz
-      },
-      {
-        path: '**', component: Error404Component // não encontrou a rota, 404
       }
     ]
     )
